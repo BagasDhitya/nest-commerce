49721e1877d04365d7e5327cd8ada4b9af37925b
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2024-05-08 17:47:01 +0700

    add: unit testing for product

---
+++ test/data/product.ts
@@ -0,0 +1,37 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 1500,
+    quantity: 10,
+    description: 'Powerful laptop for work and gaming',
+  },
+  {
+    id: 2,
+    name: 'Smartphone',
+    price: 800,
+    quantity: 20,
+    description: 'Latest smartphone with great camera',
+  },
+  {
+    id: 3,
+    name: 'Headphones',
+    price: 200,
+    quantity: 30,
+    description: 'High-quality headphones with noise cancellation',
+  },
+  {
+    id: 4,
+    name: 'Mouse Gaming',
+    price: 250,
+    quantity: 5,
+    description: 'Reliable mouse for enchance your gaming experience',
+  },
+  {
+    id: 5,
+    name: 'New Product',
+    price: 10,
+    quantity: 0,
+    description: '',
+  },
+];
